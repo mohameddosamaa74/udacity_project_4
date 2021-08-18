196eaa87a83a9cdf04796e0f4a47b2f2b9931408
Mohamed Osama <76261253+mohameddosamaa74@users.noreply.github.com>
2021-08-18 14:22:24 +0200

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,7 +5,7 @@
 
 # Step 1:
 # Create dockerpath
-dockerpath=mohameddosamaa225/udacity_microservice:v1
+dockerpath=mohameddosamaa225/udacity_microservice
 
 # Step 2:  
 # Authenticate & tag
@@ -13,8 +13,8 @@ echo "Docker ID and Image: $dockerpath"
 
 export DOCKER_ID_USER="mohameddosamaa225"
 docker login
-docker tag udacity_microservice:v1 $DOCKER_ID_USER/udacity_microservice:v1
-docker push $DOCKER_ID_USER/udacity_microservice:v1 
+docker tag udacity_microservice $DOCKER_ID_USER/udacity_microservice
+docker push $DOCKER_ID_USER/udacity_microservice
 
 # Step 3:
 # Push image to a docker repository
